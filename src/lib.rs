213f9d58481b50f7f4bcc8af7d72a66002340376
Stan Drozd <drozdziak1@gmail.com>
2018-12-06 20:02:13 +0100

    Move library code from the nip crate to nip_core

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate serde_derive;
+
+extern crate byteorder;
+extern crate env_logger;
+extern crate futures;
+extern crate git2;
+extern crate hyper;
+extern crate ipfs_api;
+extern crate serde;
+extern crate serde_cbor;
+extern crate tokio_core;
+
+pub mod constants;
+pub mod index;
+pub mod object;
+pub mod remote;
+pub mod util;
+
+pub use constants::*;
+pub use index::*;
+pub use object::*;
+pub use remote::*;
+pub use util::*;
